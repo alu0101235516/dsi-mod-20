fd4948a357c46041abbe062140e05247e3fe8637
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-05-19 11:13:49 +0100

    Repositorio inicial creado

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Funcion que retorna una suma
+ * @param a primer numero
+ * @param b segundo numero
+ * @returns suma de a y b
+ */
+export function add(a: number, b: number) {
+  return a + b;
+}
